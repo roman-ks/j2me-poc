48760cdbf42688da897d184ad9a71cd8e3a180bb
roman-ks <80638824+roman-ks@users.noreply.github.com>
2026-05-30 17:03:34 +0300

    add System.out and System.err streams to avoid NPE on use

---
+++ src/jvm-boot/java/java/io/PrintStream.java
@@ -0,0 +1,30 @@
+package java.io;
+
+public class PrintStream extends OutputStream {
+    public PrintStream() {}
+
+    public void println() {}
+    public void println(boolean x) {}
+    public void println(char x) {}
+    public void println(int x) {}
+    public void println(long x) {}
+    public void println(float x) {}
+    public void println(double x) {}
+    public void println(char[] x) {}
+    public void println(String x) {}
+    public void println(Object x) {}
+
+    public void print(boolean x) {}
+    public void print(char x) {}
+    public void print(int x) {}
+    public void print(long x) {}
+    public void print(float x) {}
+    public void print(double x) {}
+    public void print(char[] x) {}
+    public void print(String x) {}
+    public void print(Object x) {}
+
+    public void write(int b) {}
+    public void flush() {}
+    public void close() {}
+}
--- src/jvm-boot/java/java/lang/System.java
+++ src/jvm-boot/java/java/lang/System.java
@@ -1,6 +1,10 @@
 package java.lang;
 
+import java.io.PrintStream;
+
 public class System {
+    public static final PrintStream out = new PrintStream();
+    public static final PrintStream err = new PrintStream();
 
     private System() {
     }
